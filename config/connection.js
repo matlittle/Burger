bc83834cd5870a55b24ef35b9b2551047a05b113
matlittle <matlittle727@gmail.com>
2018-01-10 00:43:00 -0600

    Changed connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 
+var connection;
+
 if (process.env.JAWSDB_URL) {
-    const connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host     : 'localhost',
         user     : 'root',
         password : 'oblivion',
@@ -12,5 +14,4 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = connection;
-
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER AUTO INCREMENT,
-    PRIMARY KEY(id),
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50),
-    devoured BOOLEAN SET DEFAULT false
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY(id)
 );
